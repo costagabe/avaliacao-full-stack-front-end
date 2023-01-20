c3576a2640e32adcfaa54b878dc54109fafdaf0f
Gabriel Costa <devgabcosta@gmail.com>
2023-01-19 21:33:30 -0300

    Footer fix

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,33 +1,54 @@
 <template>
-  <v-app-bar app color="blue-darken-2" dark>
-    <v-toolbar-title>
-      <v-row justify="space-between">
+  <div class="container">
+    <v-app-bar app color="blue-darken-2" dark>
+      <v-toolbar-title>
+        <v-row justify="space-between">
 
-        <v-col cols="auto">
-          Agendador de transferências
-        </v-col>
+          <v-col cols="auto">
+            Agendador de transferências
+          </v-col>
 
-        <v-col cols="auto" class="mr-4">
-          <v-btn block color="secondary" variant="tonal">
-            <v-icon class="text-white mr-1">
-              mdi-plus
-            </v-icon>
-            <b class="text-white">Agendar</b>
-          </v-btn>
+          <v-col cols="auto" class="mr-4">
+            <v-btn block color="secondary" variant="tonal">
+              <v-icon class="text-white mr-1">
+                mdi-plus
+              </v-icon>
+              <b class="text-white">Agendar</b>
+            </v-btn>
+          </v-col>
+        </v-row>
+      </v-toolbar-title>
+    </v-app-bar>
+    <MainCard />
+   <div class="footer">
+    <v-footer class="bg-blue-darken-2">
+      <v-row justify="center" no-gutters>
+        <v-col class="text-center " cols="12">
+          {{ new Date().getFullYear() }} — <strong>Avaliação full stack</strong>
         </v-col>
       </v-row>
-    </v-toolbar-title>
-  </v-app-bar>
-  <MainCard />
-  <v-footer class="bg-blue-darken-2">
-    <v-row justify="center" no-gutters>
-      <v-col class="text-center " cols="12">
-        {{ new Date().getFullYear() }} — <strong>Avaliação full stack</strong>
-      </v-col>
-    </v-row>
-  </v-footer>
+    </v-footer>
+   </div>
+  </div>
 </template>
 
 <script setup lang="ts">
 import MainCard from "./components/MainCard.vue";
 </script>
+
+<style scoped>
+.container {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+
+.footer {
+  flex: 0 1;
+  align-self: end;
+  width: 100%;
+  display: flex;
+}
+
+</style>
